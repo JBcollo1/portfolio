192e701898d7f155e0a0f5cd88e1f95286ef4b2e
Collins <jbcollins254@gmail.com>
2024-11-04 12:41:47 +0300

    adds projec detail

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-
+import portfolio from "./service/profile.jpeg"
 
 const About = () => {
   return (
@@ -7,7 +7,7 @@ const About = () => {
         <h2 className="text-3xl font-bold text-center mb-8">About Me</h2>
         <div className="flex flex-col md:flex-row items-center">
           <div className="md:w-1/2 mb-8 md:mb-0">
-            <img src="./service/profile.jpeg" alt="Jorbat Kamau" className="rounded-full w-48 h-48 object-cover mx-auto" />
+            <img src={portfolio	} alt="Jorbat Kamau" className="rounded-full w-48 h-49 object-cover mx-auto" />
           </div>
           <div className="md:w-1/2">
             <p className="text-lg text-gray-700 leading-relaxed">
